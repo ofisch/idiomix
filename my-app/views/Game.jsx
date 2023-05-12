fbe48500a00ccdd0c175c3bd2ba20d262b0faee8
ofisch <onni.fischer@gmail.com>
2023-05-12 23:08:42 +0300

    oikein vastaaminen toimii

--- my-app/views/Game.jsx
+++ my-app/views/Game.jsx
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Start from './Start';
 
 import {selectedLang} from '../hooks/infoHooks';
-import wordData from '../src/assets/EsWords.json';
+import esWords from '../src/assets/EsWords.json';
 import ArticleBox from '../src/assets/components/ArticleBox';
 import {Box, Button, Grid, Paper, Stack, Typography} from '@mui/material';
 
@@ -11,37 +11,73 @@ const Game = () => {
   const [score, setScore] = useState(0);
   const [currentQuestion, setQurrentQuestion] = useState(0);
 
+  // palauttaa satunnaisen arvon sanalistasta
   const getRandIndex = () => {
-    return Math.floor(Math.random() * (wordData.length - 1) + 1);
+    if (selectedLang == 'es') {
+      return Math.floor(Math.random() * (esWords.length - 1) + 1);
+    } else if (selectedLang == 'se') {
+      return Math.floor(Math.random() * (se.length - 1) + 1);
+    }
   };
 
   let [questionWord, setQuestionWord] = useState('');
   let [answer, setAnswer] = useState('');
 
-  questionWord = wordData[getRandIndex()];
+  // haetaan satunnainen sana listasta
+  if (selectedLang == 'es') {
+    questionWord = esWords[getRandIndex()];
+    console.log('questionWord: ', questionWord);
+  } else if (selectedLang == 'se') {
+    console.log('ruotti kesken');
+  }
 
-  wordData[0].answer[0].article;
-
+  // tarkistetaan, onko vastaus oikein
   const checkCorrectAnswer = () => {
     let correctAnswer;
 
     for (let i of questionWord.answer) {
-      if (i.isCorrect == 'true') {
+      if (i.isCorrect) {
         correctAnswer = i.article;
       }
     }
 
     if (answer == correctAnswer) {
+      setAnswerTrue((answerTrue = true));
       setScore(score + 1);
     } else {
+      setAnswerTrue((answerTrue = false));
       setScore(score - 1);
     }
+  };
+
+  let [answerTrue, setAnswerTrue] = useState(false);
+  let [showTheAnswer, setShowTheAnswer] = useState('');
+
+  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+  let article;
+  const showCorrectAnswer = async () => {
+    questionWord.answer[0].isCorrect
+      ? (article = questionWord.answer[0].article)
+      : (article = questionWord.answer[1].article);
+    if (questionWord.type == 'article') {
+      setShowTheAnswer((showTheAnswer = article + ' ' + questionWord.word));
+    } else if (questionWord.type == 'case') {
+      setShowTheAnswer((showTheAnswer = questionWord.word + ' ' + article));
+    }
+
+    await delay(2000);
+    setAnswerTrue((answerTrue = false));
+    console.log('word: ', questionWord.word);
+    console.log('article: ', questionWord.article);
+
+    console.log('ootettu 2 sekkaa');
   };
 
   const optionClicked = (userAnswer) => {
     setAnswer((answer = userAnswer));
 
     checkCorrectAnswer();
+    showCorrectAnswer();
   };
 
   return (
@@ -56,23 +92,57 @@ const Game = () => {
           <Typography variant="h3">moi miten menee</Typography>
           <Button>Restart</Button>
         </Paper>
-      ) : (
+      ) : answerTrue ? (
         <Paper elevation={5} sx={{width: '50%', m: 'auto'}}>
-          <ArticleBox word={questionWord.word}></ArticleBox>
+          <ArticleBox word={showTheAnswer}></ArticleBox>
           <Grid
             display={'flex'}
             flexDirection={'row'}
             justifyContent={'center'}
           >
             <Button
+              variant="text"
               onClick={() => optionClicked(questionWord.answer[0].article)}
             >
-              {questionWord.answer[0].article}
+              <Typography component="h4" variant="h4">
+                {questionWord.answer[0].article}
+              </Typography>
             </Button>
             <Button
+              variant="text"
               onClick={() => optionClicked(questionWord.answer[1].article)}
             >
-              {questionWord.answer[1].article}
+              <Typography component="h4" variant="h4">
+                {questionWord.answer[1].article}
+              </Typography>
+            </Button>
+          </Grid>
+        </Paper>
+      ) : (
+        <Paper elevation={5} sx={{width: '50%', m: 'auto'}}>
+          <ArticleBox
+            word={answerTrue ? showTheAnswer : questionWord.word}
+          ></ArticleBox>
+          <Grid
+            display={'flex'}
+            flexDirection={'row'}
+            justifyContent={'center'}
+          >
+            <Button
+              variant="text"
+              onClick={() => optionClicked(questionWord.answer[0].article)}
+            >
+              <Typography component="h4" variant="h4">
+                {questionWord.answer[0].article}
+              </Typography>
+            </Button>
+            <Button
+              variant="text"
+              onClick={() => optionClicked(questionWord.answer[1].article)}
+            >
+              <Typography component="h4" variant="h4">
+                {questionWord.answer[1].article}
+              </Typography>
             </Button>
           </Grid>
         </Paper>
